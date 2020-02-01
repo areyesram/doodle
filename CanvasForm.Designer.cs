d8a16b2d14cad8345ed98e0cb07195fde8185234
Alejandro Reyes <areyram@abaton.com.mx>
2020-01-31 19:29:38 -0600

    init

---
+++ CanvasForm.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace doodle
+{
+    partial class CanvasForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CanvasForm));
+            this.toolStrip = new System.Windows.Forms.ToolStrip();
+            this.btnSave = new System.Windows.Forms.ToolStripButton();
+            this.btnColor = new System.Windows.Forms.ToolStripButton();
+            this.mnuWidth = new System.Windows.Forms.ToolStripSplitButton();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.colorDialog = new System.Windows.Forms.ColorDialog();
+            this.saveFileDialog = new System.Windows.Forms.SaveFileDialog();
+            this.toolStrip.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // toolStrip
+            // 
+            this.toolStrip.AutoSize = false;
+            this.toolStrip.Dock = System.Windows.Forms.DockStyle.Left;
+            this.toolStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.btnSave,
+            this.btnColor,
+            this.mnuWidth});
+            this.toolStrip.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip.Name = "toolStrip";
+            this.toolStrip.Size = new System.Drawing.Size(46, 626);
+            this.toolStrip.TabIndex = 4;
+            this.toolStrip.Text = "ToolStrip";
+            // 
+            // btnSave
+            // 
+            this.btnSave.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.btnSave.Image = ((System.Drawing.Image)(resources.GetObject("btnSave.Image")));
+            this.btnSave.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(44, 20);
+            this.btnSave.ToolTipText = "Save image";
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnColor
+            // 
+            this.btnColor.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.btnColor.Image = ((System.Drawing.Image)(resources.GetObject("btnColor.Image")));
+            this.btnColor.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnColor.Name = "btnColor";
+            this.btnColor.Size = new System.Drawing.Size(44, 20);
+            this.btnColor.ToolTipText = "Line color";
+            this.btnColor.Click += new System.EventHandler(this.btnColor_Click);
+            // 
+            // mnuWidth
+            // 
+            this.mnuWidth.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.mnuWidth.Image = ((System.Drawing.Image)(resources.GetObject("mnuWidth.Image")));
+            this.mnuWidth.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.mnuWidth.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.mnuWidth.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.mnuWidth.Name = "mnuWidth";
+            this.mnuWidth.Size = new System.Drawing.Size(44, 20);
+            this.mnuWidth.ToolTipText = "Line width";
+            this.mnuWidth.Click += new System.EventHandler(this.mnuWidth_Click);
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox.Location = new System.Drawing.Point(46, 0);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(811, 626);
+            this.pictureBox.TabIndex = 6;
+            this.pictureBox.TabStop = false;
+            // 
+            // saveFileDialog
+            // 
+            this.saveFileDialog.CheckFileExists = true;
+            this.saveFileDialog.Filter = "JPEG|*.jpg";
+            // 
+            // CanvasForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(857, 626);
+            this.Controls.Add(this.pictureBox);
+            this.Controls.Add(this.toolStrip);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "CanvasForm";
+            this.Text = "doodle";
+            this.toolStrip.ResumeLayout(false);
+            this.toolStrip.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ToolStrip toolStrip;
+        private System.Windows.Forms.ToolStripButton btnColor;
+        private System.Windows.Forms.ToolStripSplitButton mnuWidth;
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.ColorDialog colorDialog;
+        private System.Windows.Forms.ToolStripButton btnSave;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog;
+    }
+}
---
+++ CanvasForm.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace doodle
+{
+    public partial class CanvasForm : Form
+    {
+        private static Point _lastPoint;
+        private static bool _penDown;
+        private static Pen _currentPen = Pens.Black;
+
+        public CanvasForm()
+        {
+            InitializeComponent();
+            CreateWidthMenu();
+            CreateCanvas();
+            pictureBox.MouseDown += (sender, e) => _penDown = true;
+            pictureBox.MouseMove += (sender1, e1) =>
+            {
+                var g = Graphics.FromImage(pictureBox.Image);
+                var newPoint = new Point(e1.X, e1.Y);
+                if (_penDown)
+                    g.DrawLine(_currentPen, _lastPoint, newPoint);
+                _lastPoint = newPoint;
+                pictureBox.Image = pictureBox.Image;
+            };
+            pictureBox.MouseUp += (sender2, e2) => _penDown = false;
+        }
+
+        private void CreateWidthMenu()
+        {
+            for (var w = 1; w <= 8; w++)
+            {
+                var subMenu = new ToolStripMenuItem { Tag = w, Text = w.ToString() };
+                var width = w;
+                subMenu.Click += (o, e) => _currentPen = new Pen(_currentPen.Color, width);
+                mnuWidth.DropDownItems.Add(subMenu);
+            }
+        }
+
+        private void CreateCanvas()
+        {
+            var width = pictureBox.Width;
+            var height = pictureBox.Height;
+            pictureBox.Image = new Bitmap(width, height);
+            var g = Graphics.FromImage(pictureBox.Image);
+            g.FillRectangle(Brushes.White, new Rectangle(0, 0, width, height));
+        }
+
+        private void btnColor_Click(object sender, EventArgs e)
+        {
+            if (colorDialog.ShowDialog() == DialogResult.OK)
+                _currentPen = new Pen(colorDialog.Color, _currentPen.Width);
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if (saveFileDialog.ShowDialog() != DialogResult.OK) return;
+            var myBitmap = pictureBox.Image;
+            var encoderInfo = GetEncoderInfo("image/jpeg");
+            var @params = new EncoderParameters(1) { Param = { [0] = new EncoderParameter(Encoder.Quality, 75L) } };
+            myBitmap.Save(saveFileDialog.FileName, encoderInfo, @params);
+        }
+
+        private static ImageCodecInfo GetEncoderInfo(string mimeType)
+        {
+            return ImageCodecInfo.GetImageEncoders().FirstOrDefault(t => t.MimeType == mimeType);
+        }
+
+        private void mnuWidth_Click(object sender, EventArgs e)
+        {
+            mnuWidth.ShowDropDown();
+        }
+    }
+}
